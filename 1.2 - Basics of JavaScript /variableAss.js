dc6ce1d81ad65aba74ccd1dfe0434667f38af437
shubham09-patel <shubh200313gmail.com>
2026-06-19 19:55:44 +0530

    add assinment of variable

---
+++ 1.2 - Basics of JavaScript /variableAss.js
@@ -0,0 +1,8 @@
+//Create a variable for each of the following: your favorite color, your height in centimeters, and whether you like pizza. Use appropriate variable declarations (let, const, or var). Try logging it using console.log
+const favColor = "blue";
+const height = 165;
+const likesPizza = true;
+
+console.log(favColor);
+console.log(height);
+console.log(likesPizza);
